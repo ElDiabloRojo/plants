305c678f8455c644d865d5909d862ba686163234
ElRojoDiablo <holdens.uk@googlemail.com>
2018-10-16 23:31:40 +0100

    sketched basic graphing for plant

--- query.py
+++ query.py
@@ -2,10 +2,12 @@
 
 import json
 import MySQLdb
+import pygal
 
 from datetime import datetime
 from flask import Flask
 from flask import request
+from flask import render_template
 from flask import jsonify
 
 app = Flask(__name__)
@@ -35,6 +37,41 @@ def temp():
      print(cursor._executed)
      db.commit()
      return 'success' 
+
+@app.route('/hello/<user>')
+def hello_name(user):
+    return render_template('hello.html', name = user)
+
+
+@app.route('/pygalexample/')
+def pygalexample():
+     db = MySQLdb.connect(host='localhost',
+                          db='api',
+                          user='root',
+                          passwd='H3ll0nurse!#')
+     cursor = db.cursor()
+     sql = "SELECT moisture FROM sense_data WHERE plant = 'coffee' LIMIT 100"
+     cursor.execute(sql)
+     row = cursor.fetchall()
+     db.commit()
+     print(type(row))
+     data_points = ([int(x[0]) for x in row])
+     print(type(data_points))
+     #str =  ','.join(row)
+     #print (str)
+     #for var in row:
+     #       print (type(str(var[0])))
+
+     try:
+	graph = pygal.Line()
+	graph.title = 'coffee plant moisture over time'
+	graph.x_labels  = data_points
+        graph.add('coffee_moisture', list(data_points))
+	graph_data = graph.render_data_uri()
+        return render_template("graphing.html", graph_data = graph_data)
+     except Exception, e:
+	return(str(e))
+
 
 @app.before_request
 def log_request_info():
