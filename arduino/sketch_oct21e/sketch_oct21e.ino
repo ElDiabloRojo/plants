34e21dc3e716aae753b9a0611d19da5b9a32e7f6
Sam Holden <sah@bookboon.com>
2018-10-21 18:32:25 +0100

    code tidy

--- arduino/sketch_oct21e/sketch_oct21e.ino
+++ arduino/sketch_oct21e/sketch_oct21e.ino
@@ -25,9 +25,9 @@ String weather;
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
-  
+
   WiFi.begin(ssid, password);
- 
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting...");
@@ -47,84 +47,79 @@ void setup() {
 }
 
 void loop() {
-   DynamicJsonBuffer jsonBuffer(bufferSize);
-
-   // Check WiFi Status
-   if (WiFi.status() == WL_CONNECTED) {
-     HTTPClient weather;
-     weather.begin(apiQuery);
-     int weatherCode = weather.GET();   
-     Serial.println(weatherCode);
-                                                      
-     if (weatherCode > 0) {
-        // Parsing
-        JsonObject& weather_object = jsonBuffer.parseObject(weather.getString());
-        // Parameters
-        int datetime = weather_object["dt"];
-        int sunrise = weather_object["sys"]["sunrise"];
-        int sunset = weather_object["sys"]["sunset"];
-        String weather = weather_object["weather"]["main"];
-        Serial.print("\nWeather Object");
-        Serial.print("\ndatetime: ");
-        Serial.println(datetime);
-        Serial.print("sunrise: ");
-        Serial.println(sunrise);
-        Serial.print("sunset: ");
-        Serial.println(sunset);
-        Serial.print("weather: ");
-        Serial.println(weather); 
-        Serial.print("\n");
-     }  
-     weather.end();
-
-         
-     String schedule_check = "http://192.168.0.49/vaxer_schedule/"; 
-     HTTPClient schedule;
-     schedule.begin(schedule_check);
-     int scheduleCode = schedule.GET();
-     Serial.println(scheduleCode);
-
-     if (scheduleCode > 0) {
-     Serial.println("initialised HTTP client");                                                 
-       JsonObject& schedule_object = jsonBuffer.parseObject(schedule.getString());
-       // Parameters
-       int daylight_toggle = schedule_object["daylight"];
-       int pump_toggle = schedule_object["pump"];
-     
-       // Light
-       Serial.println("checking daylight schedule");
-       Serial.println("daylight:");
-       Serial.println(daylight_toggle);
-       if (daylight_toggle == 1) {
-       Serial.println("beginning light sequence\n");
-         
-         if (sunrise <= datetime && datetime <= sunset) {
-           digitalWrite(lightRelay,HIGH);
-           Serial.print("daylight\n");
-         }
-         else if (datetime < sunrise || datetime > sunset) {
-           digitalWrite(lightRelay,LOW);
-           Serial.print("nighttime\n");
-         }
-         else {
-           Serial.println("no data\n");
-         }
-     }
-     schedule.end();
-     // Pump
-     Serial.println("pump:");
-     Serial.println(pump_toggle);
-     Serial.println("checking pump schedule");
-     if (pump_toggle == 1) {
-       Serial.println("beginning pump sequence\n");
-       digitalWrite(pumpRelay, HIGH);
-       Serial.print("pumping\n");
-       delay(60000);
-       digitalWrite(pumpRelay, LOW);
-       Serial.print("end pumping\n"); 
-     } 
-     }
+  DynamicJsonBuffer jsonBuffer(bufferSize);
+  // Check WiFi Status
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient weather;
+    weather.begin(apiQuery);
+    int weatherCode = weather.GET();
+    Serial.println(weatherCode);
+    if (weatherCode > 0) {
+      // Parsing
+      Serial.println(weather.getString());
+      JsonObject& weather_object = jsonBuffer.parseObject(weather.getString());
+      // Parameters
+      int datetime = weather_object["dt"];
+      int sunrise = weather_object["sys"]["sunrise"];
+      int sunset = weather_object["sys"]["sunset"];
+      String weather = weather_object["weather"]["main"];
+      Serial.print("\nWeather Object");
+      Serial.print("\ndatetime: ");
+      Serial.println(datetime);
+      Serial.print("sunrise: ");
+      Serial.println(sunrise);
+      Serial.print("sunset: ");
+      Serial.println(sunset);
+      Serial.print("weather: ");
+      Serial.println(weather);
+      Serial.print("\n");
+      weather.end();
+    }
+    String schedule_check = "http://192.168.0.49/vaxer_schedule/";
+    HTTPClient schedule;
+    schedule.begin(schedule_check);
+    int scheduleCode = schedule.GET();
+    Serial.println(scheduleCode);
+    if (scheduleCode > 0) {
+      Serial.println("initialised HTTP client");
+      Serial.println(schedule.getString());
+      JsonObject& schedule_object = jsonBuffer.parseObject(schedule.getString());
+      // Parameters
+      int daylight_toggle = schedule_object["daylight"];
+      int pump_toggle = schedule_object["pump"];
+      // Light
+      Serial.println("checking daylight schedule");
+      Serial.println("daylight:");
+      Serial.println(daylight_toggle);
+      if (daylight_toggle == 1) {
+        Serial.println("beginning light sequence\n");
+        if (sunrise <= datetime && datetime <= sunset) {
+          digitalWrite(lightRelay,HIGH);
+          Serial.print("daylight\n");
+        }
+        else if (datetime < sunrise || datetime > sunset) {
+          digitalWrite(lightRelay,LOW);
+          Serial.print("nighttime\n");
+        }
+        else {
+          Serial.println("no data\n");
+        }
+      }
+    schedule.end();
+    }
+    // Pump
+    Serial.println("pump:");
+    Serial.println(pump_toggle);
+    Serial.println("checking pump schedule");
+    if (pump_toggle == 1) {
+      Serial.println("beginning pump sequence\n");
+      digitalWrite(pumpRelay, HIGH);
+      Serial.print("pumping\n");
+      delay(60000);
+      digitalWrite(pumpRelay, LOW);
+      Serial.print("end pumping\n");
+    }
   Serial.print("end cycle...\n");
   delay(120000);
-}
+  }
 }
